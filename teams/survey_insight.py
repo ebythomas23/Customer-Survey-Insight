b6d0b2c24201027100ed397f70b9626ad7b8698b
eby <ebykachappillil@gmail.com>
2025-11-18 20:43:47 +0530

    Add survey insight team with RoundRobin chat

---
+++ teams/survey_insight.py
@@ -0,0 +1,14 @@
+from agents.SurveyInsightAgent import getSurveyInsightAgent
+from autogen_agentchat.conditions import TextMentionTermination
+from autogen_agentchat.teams import RoundRobinGroupChat
+
+
+async def get_survey_insight_team():
+    agent=  await getSurveyInsightAgent()
+
+    team = RoundRobinGroupChat(
+            participants=[agent],
+            max_turns=4,
+            termination_condition=TextMentionTermination('STOP')
+        )
+    return team
